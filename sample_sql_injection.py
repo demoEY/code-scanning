3bfd6763170b8d2afd2e9a4d268b3df899018b56
demoEY <125969644+demoEY@users.noreply.github.com>
2023-06-21 22:25:50 +0530

    Update sample_sql_injection.py

--- sample_sql_injection.py
+++ sample_sql_injection.py
@@ -1,7 +1,8 @@
 @app.route("/login")
+password = "123&4"
 def login():
   username = request.values.get('username')
-  password = request.values.get('password')
+  password = password
   db = pymysql.connect("localhost")
   cursor = db.cursor()
   cursor.execute("SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (username, password))
