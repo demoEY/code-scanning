4947534765ed241990f5c5a1488855b446c36f01
demoEY <125969644+demoEY@users.noreply.github.com>
2023-08-29 11:15:44 +0530

    Update sample_sql_injection.py

--- sample_sql_injection.py
+++ sample_sql_injection.py
@@ -5,8 +5,8 @@ def login():
   db = pymysql.connect("localhost")
   cursor = db.cursor()
   print(username)
-  # cursor.execute("SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (username, password))
-  # cursor.execute(f"SELECT admin FROM users WHERE username = '{username}'");
+  cursor.execute("SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (username, password))
+  cursor.execute(f"SELECT admin FROM users WHERE username = '{username}'");
   record = cursor.fetchone()
   session['logged_user'] = username
   db.close()
